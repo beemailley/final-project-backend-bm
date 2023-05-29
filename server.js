ed748bbaf1389777f96dc9cad7a6c232fa53eecd
Fiona Klacar <fiona.klacar@gmail.com>
2023-05-29 20:58:44 +0200

    drafted schema, set up dependencies

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose"; 
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+
+
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project-users";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -15,6 +26,61 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+const UserSchema = new mongoose.Schema ({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    
+  },
+  lastName: {
+
+  },
+  emailAddress: {
+
+  },
+  memberSince: {
+    type: Date,
+    default: () => new Date()
+  },
+  gender: {
+    type: String
+  },
+  birthday: {
+    type: Date
+  },
+  interests: {
+    type: String
+  },
+  currentCity: {
+    type: String,
+    required: true
+  },
+  homeCountry: {
+    type: String
+  },
+  languages: {
+    type: String
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+
+})
+
+const User = mongoose.model("User", UserSchema);
+
+
+
 
 // Start the server
 app.listen(port, () => {
