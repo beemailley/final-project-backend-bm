3f142feb29c39f3ec943d0c6b6ee856ae74750e6
Fiona Klacar <fiona.klacar@gmail.com>
2023-05-30 09:52:07 +0200

    updated schema, added a dependency to obtain a country list for homeCountry property, created register and login endpoints

--- server.js
+++ server.js
@@ -42,17 +42,23 @@ const UserSchema = new mongoose.Schema ({
     
   },
   lastName: {
+    type: String,
+    minlength: 2,
 
   },
   emailAddress: {
-
+    type: String,
+    unique: true,
+    match: [/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/, 'Please enter a valid email address']
   },
   memberSince: {
     type: Date,
     default: () => new Date()
   },
   gender: {
-    type: String
+    type: String,
+    enum: ['male', 'female', 'non-binary', 'other', 'prefer not to say'],
+    default: 'prefer not to say'
   },
   birthday: {
     type: Date
@@ -62,10 +68,11 @@ const UserSchema = new mongoose.Schema ({
   },
   currentCity: {
     type: String,
-    required: true
+    enum: ['Stockholm', 'London']
   },
   homeCountry: {
-    type: String
+    type: String,
+    enum: countryList.getNames()
   },
   languages: {
     type: String
@@ -79,8 +86,66 @@ const UserSchema = new mongoose.Schema ({
 
 const User = mongoose.model("User", UserSchema);
 
+app.post("/register", async (req, res) => {
+  const { username, password} = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (user) {
+      res.status(400).json({
+        success: false,
+        response: "Username already exists,"
+      })
+    } else {
+      const salt = bcrypt.genSaltSync();
+      const newUser = await new User({
+        username: username,
+        password: bcrypt.hashSync(password, salt)
+      }).save();
+      res.status(201).json({
+        success: true,
+        response: {
+          username: newUser.username,
+          id: newUser._id,
+          accessToken: newUser.accessToken
+        }
+    })
+  }
+} catch (e) {
+  console.log(e);
+    res.status(400).json({
+      success: false,
+      response: "Registration failed. Please try again."
+    })
+  }
+});
 
-
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({username: username})
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken
+    }
+  });
+  } else {
+      res.status(400).json({
+        success: false,
+        response: "Credentials do not match"
+    });
+  } 
+} catch (e) {
+      console.log(e)
+        res.status(400).json({
+        success: false,
+        response: e
+    })
+  }
+});
 
 // Start the server
 app.listen(port, () => {
